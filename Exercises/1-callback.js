84e87ce776898b6341d9feff0fe6efb13ed056c6
GhostDolphin <d.ghost2ac@gmail.com>
2021-11-01 14:29:18 +0200

    HigherOrderFunctions ALL taska

--- Exercises/1-callback.js
+++ Exercises/1-callback.js
@@ -1,5 +1,19 @@
 'use strict';
 
-const iterate = (obj, callback) => null;
+//for in
+
+/*const iterate = (obj, callback) => {
+  for (const key in obj) {
+    callback(key, obj[key]);
+  }
+};*/
+
+const iterate = (obj, callback) => {
+  const keys = Object.keys(obj);
+  for (const key of keys) {
+    const value = obj[key];
+    callback(key, value, obj);
+  }
+};
 
 module.exports = { iterate };
--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,21 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const getType = arg => typeof arg;
+
+const contract = (fn, ...types) => (...args) => {
+  const typesOrder = args.map(getType);
+  for (let i = 0; i < typesOrder.length; i++) {
+    const rightType = types[i].name.toLowerCase();
+    const checkType = typesOrder[i];
+    if (checkType !== rightType)
+      throw new TypeError(`Argument #${i + 2} type ${rightType} expected`);
+  }
+  const res = fn(...args);
+  const rightType = types[types.length - 1].name.toLowerCase();
+  const checkType = getType(res);
+  if (checkType !== rightType)
+    throw new TypeError(`Argument #${types.length + 1} type ${rightType} expected`);
+  return res;
+};
 
 module.exports = { contract };
